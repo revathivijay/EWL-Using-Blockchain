d825e27f311997622e4456586736c7ac26e0c017
Ritu Gala <39478730+ritugala@users.noreply.github.com>
2021-04-12 21:00:28 +0530

    Latest version of database

--- Portal/generateRanklist.py
+++ Portal/generateRanklist.py
@@ -15,16 +15,16 @@ gradedReports = mongo.db.reports
 jobs = mongo.db.jobs
 # students.update_many({}, {"$set": {"impactScore":0}})
 # research.update({}, {"$unset": {"impactFactor":-1}})
-research.update({"_id":ObjectId('5fd04dce0d98dceeddf66ad7')}, {"$set":{"file_list":{"report1.pdf":False}}})
-research.update({"_id":ObjectId('5fd3121c5da308971f9b2aed')}, {"$set":{"file_list":{"report1.pdf":False}}})
+# research.update({"_id":ObjectId('5fd04dce0d98dceeddf66ad7')}, {"$set":{"file_list":{"report1.pdf":False}}})
+# research.update({"_id":ObjectId('5fd3121c5da308971f9b2aed')}, {"$set":{"file_list":{"report1.pdf":False}}})
 # research.update_one({"_id":ObjectId('5fd04dce0d98dceeddf66ad7')}, {"$set":{"staff":["1"]}})
 
-research.update({"_id":ObjectId('5fd335f349359305f83f9d04')}, {"$set":{"publicationDOI":"10.1145/3327960.3332389", "publicationJournal":"Future Generation Computer Systems-The International Journal of eScience", "isPublished":False}})
+# research.update({"_id":ObjectId('5fd335f349359305f83f9d04')}, {"$set":{"publicationDOI":"10.1145/3327960.3332389", "publicationJournal":"Future Generation Computer Systems-The International Journal of eScience", "isPublished":False}})
 # # cursor = research.find({})
 # report1 = {"projectID":"5fd04dce0d98dceeddf66ad7", "reportName":"report1.pdf", "effort": None, "relevance":None, "novelty":None}
 # report2 = {"projectID":"5fd04dce0d98dceeddf66ad7", "reportName":"report2.pdf", "effort": None, "relevance":None, "novelty":None}
 # gradedReports.insert_many([report1, report2])
-cursor = jobs.find({})
+cursor = gradedReports.find({})
 #one time insertion
 for document in cursor:
-      print(document)
+	print(document)
