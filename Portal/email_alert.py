4d239c483aea01eb0d2bf8533d866b2508ee8eb2
revathivijay <34963337+revathivijay@users.noreply.github.com>
2021-04-16 16:13:29 +0530

    email logic completed, trigger incomplete

--- Portal/email_alert.py
+++ Portal/email_alert.py
@@ -2,6 +2,7 @@ import smtplib
 from Portal.routes import app
 from flask_pymongo import PyMongo, MongoClient
 from bson import ObjectId
+from routes import mongo
 
 # LOGIN CREDENTIALS
 gmail_user = 'testvjchain@gmail.com'
@@ -12,34 +13,34 @@ DEFAULT_SUBJECT = "Reminder for Upcoming Project Deadline"
 rev_email = 'revathivijay99@gmail.com'
 ritu_email = 'ritugala13@gmail.com'
 
-# SETTING UP MONGO
-mongo = PyMongo(app)
-students = mongo.db.students
-research = mongo.db.research
-staff = mongo.db.staff
-gradedReports = mongo.db.reports
-jobs = mongo.db.jobs
 
+def send_email(sender, pwd, user_id, subject=DEFAULT_SUBJECT):
 
-def send_email(sender, pwd, recipient, user_id, subject=DEFAULT_SUBJECT):
-
-    FROM = sender
-    TO = recipient if isinstance(recipient, list) else [recipient]
-    SUBJECT = subject
-
+    # getting project info from db
     project = mongo.db.research.find_one({"_id": ObjectId(user_id)})
     topic = project['topic']
-    mentor = project['faculty']  # is this correct
+    mentors = project['staff']
+    students_in_project = project['students']
+    recipients = []
+
+    # getting student email IDs
+    for student in students_in_project:
+        student_details = mongo.db.students.find_one({"id": student})
+        recipients.append(student_details['email'])
+
     # date = project['start_date']  # to be added
     date = '10 Apr 2021'
 
+    # setting up email parameters
+    FROM = sender
+    TO = recipients if isinstance(recipients, list) else [recipients]
+    SUBJECT = subject
+
     # construct body of email
-    TEXT = f'''Your project titled {topic} under mentor {mentor} has an upcoming deadline of {date}. 
-        Please submit your biweekly report before this deadline.'''
+    TEXT = f'''Your project titled "{topic}" under the mentor(s) {", ".join(mentors)} has an upcoming deadline of {date}. Please submit your biweekly report before this deadline.'''
 
     # construct message
     message = f"""From: {FROM}\nTo: {", ".join(TO)}\nSubject: {SUBJECT}\n\n{TEXT}"""
-
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.ehlo()
@@ -53,8 +54,5 @@ def send_email(sender, pwd, recipient, user_id, subject=DEFAULT_SUBJECT):
         print ("Oops! Something went wrong. Mail not sent!")
 
 
-# def trigger_send(user, project, date): ##TODO
-#     if today less than 1 week away from date for user:
-#         send_email(gmail_user, gmail_password, )
-
-send_email(gmail_user, gmail_password, rev_email, '5fd335f349359305f83f9d04')
+#TODO: WRITE TRIGGER FOR THIS FUNCTION
+send_email(gmail_user, gmail_password, '5fd04d720d98dceeddf66ad6')
